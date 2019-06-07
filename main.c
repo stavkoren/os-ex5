4803d21c564524076a92183a8e708ba1553d304b
stavkoren <44188648+stavkoren@users.noreply.github.com>
2019-06-07 19:49:51 +0300

    add alerts

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define FRAME_SIZE 20
 #define BORDER_CHAR '*'
-#define SHAPE_CHAR'-'
+#define SHAPE_CHAR '-'
 #define SPACE_CHAR ' '
 #define UNDER_LINE "\n"
 #define FIRST_ROW 0
@@ -12,6 +15,7 @@
 #define INDEX_NEXT 1
 #define INIT_ROW 1
 #define MIDDLE 2
+#define ALARM_INTERVAL 1
 struct point{
     int row;
     int column;
@@ -22,6 +26,7 @@ struct shape{
 };
 char board[FRAME_SIZE][FRAME_SIZE];
 struct shape shape;
+bool runProg=true;
 /**
  * draw board to screen
  */
@@ -100,12 +105,92 @@ void moveShapeDown(){
         initShape();
     }
 }
-
-int main() {
-    fillFrame();
-    initShape();
+/**
+ * move shape one index left if possible
+ */
+void moveShapeLeft(){
+    if(shape.beginPoint.column>FIRST_COLUMN+INDEX_NEXT){
+        shape.beginPoint.column=shape.beginPoint.column+INDEX_BACK;
+        shape.endPoint.column=shape.endPoint.column+INDEX_BACK;
+    }
+}
+/**
+ * move shape one index right if possible
+ */
+void moveShapeRight(){
+    if(shape.endPoint.column<FRAME_SIZE+INDEX_BACK){
+        shape.beginPoint.column=shape.beginPoint.column+INDEX_NEXT;
+        shape.endPoint.column=shape.endPoint.column+INDEX_NEXT;
+    }
+}
+/**
+ * rotate shape is possible
+ */
+void rotateShape(){
+    if (isVertical()){
+        shape.beginPoint.row+=INDEX_NEXT;
+        shape.endPoint.row+=INDEX_BACK;
+        shape.beginPoint.column+=INDEX_BACK;
+        shape.endPoint.column+=INDEX_NEXT;
+    }else{
+        //the shape is horizontal
+        if(shape.endPoint.row<FRAME_SIZE+INDEX_BACK){
+            shape.beginPoint.row+=INDEX_BACK;
+            shape.endPoint.row+=INDEX_NEXT;
+            shape.beginPoint.column+=INDEX_NEXT;
+            shape.endPoint.column+=INDEX_BACK;
+        }
+    }
+}
+/**
+ * clear screen
+ */
+void clearScreen(){
+   // system("clear");
+    system("@cls||clear");
+}
+/**
+ * exit program
+ */
+void exitProgram(){
+    close(STDIN_FILENO);
+    exit(EXIT_SUCCESS);
+}
+/**
+ * delete shape from board
+ */
+void deleteShape(){
+    if(isVertical()){
+        for (int i=shape.beginPoint.row;i<=shape.endPoint.row;i++){
+            board[i][shape.endPoint.column]=SPACE_CHAR;
+        }
+    }else{
+        //the shape is horizontal
+        for (int i=shape.beginPoint.column;i<=shape.endPoint.column;i++){
+            board[shape.endPoint.row][i]=SPACE_CHAR;
+        }
+    }
+}
+/**
+ * alarm procedure- move shape down on board
+ * @param sig
+ */
+void alarmHand(int sig){
+    clearScreen();
+    deleteShape();
     moveShapeDown();
     markShapeOnBord();
     drawBoard();
+    signal(SIGALRM,alarmHand);
+    alarm(ALARM_INTERVAL);
+}
+int main() {
+    fillFrame();
+    initShape();
+    signal(SIGALRM,alarmHand);
+    alarm(ALARM_INTERVAL);
+    while (runProg){
+        pause();
+    }
     return 0;
 }
